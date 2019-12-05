04fa025291736317636ba25a6a9d3d72ebe8ebd9
bmccombs369 <bmccombs369@gmail.com>
2019-12-04 19:02:17 -0500

    built setStatusClosed function

--- scripts/main.js
+++ scripts/main.js
@@ -30,25 +30,41 @@ function saveIssue(e) {
   e.preventDefault();
 }
 
+function setStatusClosed(id) {
+  let issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
 function fetchIssues() {
   let issues = JSON.parse(localStorage.getItem('issues'));
   let issuesList = document.getElementById('issuesList');
 
   issuesList.innerHTML = '';
 
-  for (var i = 0; i <issues.length; i++) {
+  for (let i = 0; i <issues.length; i++) {
     let id = issues[i].id;
     let description = issues[i].description;
     let severity = issues[i].severity;
     let status = issues[i].status;
 
-    issuesList.innerHTML += '<div class="well">' +
+    issuesList.innerHTML += '<div class="card">' +
+                            '<div class="card-body">' +
                             '<h6>Issue ID: ' + id + '</h6>' +
                             '<p><span class="label label-info">' + status + '</span></p>' +
                             '<h3>' + description + '</h3>' +
                             '<p><span class"glyphicon glyphicon-time></span>' + severity + '</p>' +
                             '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' +
                             '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                            '</div>' +
                             '</div>'
   }
 }
